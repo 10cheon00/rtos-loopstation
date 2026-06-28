1ca1529cd5699918579b7f7dd753577c17737340
10cheon00 <10cheon00@gmail.com>
2026-06-28 21:26:28 +0900

    chore: cmake관련 파일 추가

---
+++ cmake/stm32cubemx/CMakeFiles/FatFs.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FatFs.dir/__/__/Middlewares/Third_Party/FatFs/src/diskio.c.o"
+  "CMakeFiles/FatFs.dir/__/__/Middlewares/Third_Party/FatFs/src/diskio.c.o.d"
+  "CMakeFiles/FatFs.dir/__/__/Middlewares/Third_Party/FatFs/src/ff.c.o"
+  "CMakeFiles/FatFs.dir/__/__/Middlewares/Third_Party/FatFs/src/ff.c.o.d"
+  "CMakeFiles/FatFs.dir/__/__/Middlewares/Third_Party/FatFs/src/ff_gen_drv.c.o"
+  "CMakeFiles/FatFs.dir/__/__/Middlewares/Third_Party/FatFs/src/ff_gen_drv.c.o.d"
+  "CMakeFiles/FatFs.dir/__/__/Middlewares/Third_Party/FatFs/src/option/syscall.c.o"
+  "CMakeFiles/FatFs.dir/__/__/Middlewares/Third_Party/FatFs/src/option/syscall.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FatFs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
