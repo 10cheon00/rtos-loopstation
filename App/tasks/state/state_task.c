869de8fc5465026966142c4b031223dd917491cb
10cheon00 <10cheon00@gmail.com>
2026-07-13 15:34:41 +0900

    chore: 상태 관리 태스크 초기 코드 생성

---
+++ App/tasks/state/state_task.c
@@ -0,0 +1,5 @@
+#include "state_task.h"
+
+void StateTask_Init(void) {}
+
+void StateTask_Run(void) {}
---
+++ App/tasks/state/state_task.h
@@ -0,0 +1,15 @@
+#ifndef STATE_TASK_H
+#define STATE_TASK_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void StateTask_Init(void);
+void StateTask_Run(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
