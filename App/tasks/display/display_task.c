8b6f93409feac009bd0317e04dbfda694206abfa
10cheon00 <10cheon00@gmail.com>
2026-07-20 16:47:42 +0900

    feat: UiStateRenderPayload에 따라 다른 화면을 출력하는 테스트 코드 추가

--- App/tasks/display/display_task.c
+++ App/tasks/display/display_task.c
@@ -3,17 +3,20 @@
 #include "cmsis_os2.h"
 #include "gmg12864_lcd.h"
 
+#include "display_messages.h"
 #include "display_initparams.h"
 
 static u8g2_t u8g2;
 extern const uint8_t u8g2_font_ref4x5_prop_v4_tr[];
 
+static osMessageQueueId_t display_command_queue;
+
 static int DisplayTask_IsValidInitParams(const DisplayInitParams *params)
 {
-    return (params != 0) && (params->display_command_queue != 0 && params->hspi != NULL &&
-                             params->CS_Pin != 0 && params->CS_Port != NULL &&
-                             params->RST_Pin != 0 && params->RST_Port != NULL &&
-                             params->DC_Pin != 0 && params->DC_Port != NULL);
+    return (params != 0) &&
+           (params->display_command_queue != 0 && params->hspi != NULL && params->CS_Pin != 0 &&
+            params->CS_Port != NULL && params->RST_Pin != 0 && params->RST_Port != NULL &&
+            params->DC_Pin != 0 && params->DC_Port != NULL);
 }
 
 void DisplayTask_Init(void *argument)
@@ -27,16 +30,15 @@ void DisplayTask_Init(void *argument)
         }
     }
 
-    Gmg12864Lcd_InitParams initparams = {
-        .hspi = params->hspi,
-        .CS_Pin = params->CS_Pin,
-        .CS_Port = params->CS_Port,
-        .DC_Pin = params->DC_Pin,
-        .DC_Port = params->DC_Port,
-        .RST_Pin = params->RST_Pin,
-        .RST_Port = params->RST_Port
-    };
-    ;
+    display_command_queue = params->display_command_queue;
+
+    Gmg12864Lcd_InitParams initparams = {.hspi = params->hspi,
+                                         .CS_Pin = params->CS_Pin,
+                                         .CS_Port = params->CS_Port,
+                                         .DC_Pin = params->DC_Pin,
+                                         .DC_Port = params->DC_Port,
+                                         .RST_Pin = params->RST_Pin,
+                                         .RST_Port = params->RST_Port};
 
     Gmg12864LcdStatus status = Gmg12864Lcd_Init(&u8g2, &initparams);
     if (status != GMG12864_LCD_STATUS_OK) {
@@ -51,10 +53,24 @@ void DisplayTask_Init(void *argument)
 
 void DisplayTask_Run(void)
 {
+    DisplayCommand command;
     u8g2_SetFont(&u8g2, u8g2_font_ref4x5_prop_v4_tr);
     u8g2_ClearBuffer(&u8g2);
     u8g2_DrawStr(&u8g2, 6, 6, "hi666!");
     u8g2_SendBuffer(&u8g2);
     for (;;) {
+        osMessageQueueGet(display_command_queue, &command, NULL, osWaitForever);
+        if (command.type == DISPLAY_COMMAND_UI_STATE_RENDER) {
+            if (command.payload.ui_state_render.panel_id == 1) {
+                u8g2_ClearBuffer(&u8g2);
+                u8g2_DrawStr(&u8g2, 6, 6, "HOME PANEL");
+                u8g2_SendBuffer(&u8g2);
+            }
+            if (command.payload.ui_state_render.panel_id == 2) {
+                u8g2_ClearBuffer(&u8g2);
+                u8g2_DrawStr(&u8g2, 6, 6, "SETTING PANEL");
+                u8g2_SendBuffer(&u8g2);
+            }
+        }
     }
 }
