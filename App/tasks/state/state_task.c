a6e2acbbda8c270cd376aa24b94fb2e70a48ba0e
10cheon00 <10cheon00@gmail.com>
2026-07-23 00:26:04 +0900

    fix: 발생한 이벤트에 따라 여러 상태 머신이 전이될 수 있으므로 443077b 커밋을 롤백

--- App/tasks/state/state_task.c
+++ App/tasks/state/state_task.c
@@ -17,7 +17,6 @@ static DisplayPanelContext display_panel_context;
 static StateMachine display_state_machine;
 
 static TaskStatus StateTask_DispatchStateEvent(const StateEvent *state_event);
-static TaskStatus StateTask_HandleStateEvent(StateMachine *state_machine, StateEvent *state_event);
 
 static int StateTask_IsValidInitParams(const StateInitParams *params)
 {
@@ -72,35 +71,21 @@ void StateTask_Run(void)
 static TaskStatus StateTask_DispatchStateEvent(const StateEvent *state_event)
 {
     EventHandlingResult result;
-    TaskStatus task_status;
 
     switch (state_event->type) {
     case STATE_EVENT_CONTROL_BUTTON:
         if (state_event->payload.control_button.state == CONTROL_BUTTON_STATE_RELEASED) {
-            task_status = StateTask_HandleEvent(&display_state_machine, state_event);
+            result = display_state_machine.current_state->on_event(state_event);
         }
         break;
     default:
         break;
     }
 
-    if (task_status == TASK_STATUS_ERROR) {
-        return TASK_STATUS_ERROR;
-    }
-
-    return TASK_STATUS_OK;
-}
-
-static TaskStatus StateTask_HandleStateEvent(StateMachine *state_machine, StateEvent *state_event)
-{
-    EventHandlingResult result = state_machine->current_state->on_event(state_event);
-
     if (result.status == EVENT_HANDLING_STATUS_TRANSITION) {
-        StateTransition transition = {
-            .cause_event = state_event, .to = result.next_state, .context = state_machine->context};
-        StateMachine_DoTransition(state_machine, &transition);
-    } else if (result.status == EVENT_HANDLING_STATUS_ERROR) {
-        return TASK_STATUS_ERROR;
+        StateTransition transition = {.cause_event = state_event, .to = result.next_state, .context = &display_panel_context};
+        StateMachine_DoTransition(&display_state_machine, &transition);
     }
+
     return TASK_STATUS_OK;
 }
