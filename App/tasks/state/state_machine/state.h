ff4de97299deb75c2753d6f8aa4cb94a129b1861
10cheon00 <10cheon00@gmail.com>
2026-07-25 15:09:16 +0900

    refactor: 네임 컨벤션을 지키도록 state.h의 struct, enum 타입명 수정

--- App/tasks/state/state_machine/state.h
+++ App/tasks/state/state_machine/state.h
@@ -6,26 +6,26 @@
 typedef struct State State;
 
 typedef enum {
-    EVENT_HANDLING_STATUS_HANDLED,
-    EVENT_HANDLING_STATUS_TRANSITION,
-    EVENT_HANDLING_STATUS_IGNORED,
-    EVENT_HANDLING_STATUS_REJECTED,
-    EVENT_HANDLING_STATUS_ERROR
-} EventHandlingStatus;
+    STATE_ON_EVENT_HANDLING_STATUS_HANDLED,
+    STATE_ON_EVENT_HANDLING_STATUS_TRANSITION,
+    STATE_ON_EVENT_HANDLING_STATUS_IGNORED,
+    STATE_ON_EVENT_HANDLING_STATUS_REJECTED,
+    STATE_ON_EVENT_HANDLING_STATUS_ERROR
+} StateOnEventHandlingStatus;
 
 typedef struct {
     const State *next_state;
-    EventHandlingStatus status;
-} EventHandlingResult;
+    StateOnEventHandlingStatus status;
+} StateOnEventHandlingResult;
 
-typedef void(*OnEnterFunction)(void* context);
-typedef EventHandlingResult(*OnEventFunction)(const StateEvent* event);
-typedef void(*OnExitFunction)(void);
+typedef void(*StateOnEnterFunction)(void* context);
+typedef StateOnEventHandlingResult(*StateOnEventFunction)(const StateEvent* event);
+typedef void(*StateOnExitFunction)(void);
 
 struct State {
-    OnEnterFunction on_enter;
-    OnEventFunction on_event;
-    OnExitFunction on_exit;
+    StateOnEnterFunction on_enter;
+    StateOnEventFunction on_event;
+    StateOnExitFunction on_exit;
     void *context;
     const uint16_t id; // TODO: enum으로 나타낸 상태 ID를 uint16_t로 취급해도 되는지 결정하기
 };
