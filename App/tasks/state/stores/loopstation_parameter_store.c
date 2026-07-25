846241bfcee691539737291018507d7e421400c0
10cheon00 <10cheon00@gmail.com>
2026-07-25 15:01:27 +0900

    feat: 루프스테이션의 모든 파라미터 값을 저장하는 싱글톤 저장소 추가

---
+++ App/tasks/state/stores/loopstation_parameter_store.c
@@ -0,0 +1,16 @@
+#include "loopstation_parameter_store.h"
+
+static LoopStationParameterStore loopstation_parameter_store = {
+    .parameters = {
+        [PARAMETER_ID_SETTING_PANEL_CURSOR] =
+            {
+                .min = 0,
+                .max = 1,
+                .current = 0,
+            },
+    }};
+
+LoopStationParameterStore *LoopstationParameterStore_GetInstance()
+{
+    return &loopstation_parameter_store;
+}
---
+++ App/tasks/state/stores/loopstation_parameter_store.h
@@ -0,0 +1,19 @@
+#ifndef LOOPSTATION_PARAMETER_STORE_H
+#define LOOPSTATION_PARAMETER_STORE_H
+
+#include "ui_state_parameter_binding.h"
+
+/**
+ * TODO: 
+ * 파라미터 수가 많지 않으므로 하나의 저장소에서 모든 파라미터를 관리한다.
+ * 아직 다른 패널들이 개발되지 않았으므로 하나의 저장소로 충분하다.
+ * 단, 파라미터 수가 50개 이상이 되는 경우 패널별로 분리해야한다.
+ * 저장소를 분리한다면 ParameterId도 저장소에 따라 분리해야한다.
+ */
+typedef struct {
+    Parameter parameters[PARAMETER_ID_COUNT];
+} LoopStationParameterStore;
+
+LoopStationParameterStore* LoopstationParameterStore_GetInstance();
+
+#endif
