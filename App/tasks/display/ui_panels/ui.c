beec90a46e0a87e3ea92b1538a04a47b00ac131e
10cheon00 <10cheon00@gmail.com>
2026-07-30 23:50:14 +0900

    fix: 숫자를 문자열로 바꿔서 렌더링하도록 유틸함수 구현

--- App/tasks/display/ui_panels/ui.c
+++ App/tasks/display/ui_panels/ui.c
@@ -15,3 +15,24 @@ void UI_DrawArrowRight4x5(u8g2_t *u8g2, uint8_t x, uint8_t y)
     u8g2_DrawVLine(u8g2, x + 2, y - 4, 3);
     u8g2_DrawVLine(u8g2, x + 3, y - 3, 1);
 }
+
+void UI_ConvertNumberToString(int32_t number, char *string, uint8_t string_length)
+{
+    uint8_t i = string_length - 1, j, number_length = 0, is_negative = number < 0 ? 1 : 0;
+
+    string[i] = '\0';
+    i--;
+    string[i] = '0';
+    while (i >= 0) {
+        if (number == 0) {
+            if (is_negative) {
+                string[i] = '-';
+            }
+            break;
+        }
+        string[i] = number % 10 + '0';
+        i--;
+        number_length++;
+        number /= 10;
+    }
+}
--- App/tasks/display/ui_panels/ui.h
+++ App/tasks/display/ui_panels/ui.h
@@ -12,5 +12,5 @@ extern const uint8_t u8g2_font_ref4x5_prop_v4_tr[];
 
 void UI_DrawArrowLeft4x5(u8g2_t *u8g2, uint8_t x, uint8_t y);
 void UI_DrawArrowRight4x5(u8g2_t *u8g2, uint8_t x, uint8_t y);
-
+void UI_ConvertNumberToString(int32_t number, char* string, uint8_t string_length);
 #endif
