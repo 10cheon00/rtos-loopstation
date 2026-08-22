df8d712b6fea1c976e1bf9ba8e16464993488237
10cheon00 <10cheon00@gmail.com>
2026-08-22 21:04:55 +0900

    fix: 쓰지 않는 패널 파라미터 테이블 제거

--- App/tasks/state/ui_state_machine/ui_state_machine.c
+++ App/tasks/state/ui_state_machine/ui_state_machine.c
@@ -2,7 +2,6 @@
 
 #include "display_messages.h"
 #include "ui_panel_ui_state_config_table.h"
-#include "panel_parameter_config_table.h"
 #include "loopstation_parameter_store.h"
 
 void UiStateMachine_Init(UiStateMachine *ui_state_machine, UiStateMachineContext *context,
