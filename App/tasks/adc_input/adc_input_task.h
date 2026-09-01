a51e4102e3c4c5ae234ba2da08be26f36f0d5bd6
10cheon00 <10cheon00@gmail.com>
2026-09-01 14:02:14 +0900

    refactor: App/ 하위의 모든 C 파일(라이브러리 제외)들의 확장자를 cpp로 수정

--- App/tasks/adc_input/adc_input_task.h
+++ App/tasks/adc_input/adc_input_task.h
@@ -1,9 +1,17 @@
 #ifndef ADC_INPUT_TASK_H
 #define ADC_INPUT_TASK_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "app.h"
 
-void AdcInputTask_Init(void* arguments);
-void AdcInputTask_Run();
+void AdcInputTask_Init(void *arguments);
+void AdcInputTask_Run(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
